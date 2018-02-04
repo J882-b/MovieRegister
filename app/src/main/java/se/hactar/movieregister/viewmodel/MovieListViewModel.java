62912612a48673dfd84a1c8a13abbeca70d7da77
Johan Hallenberg <johan@hactar.se>
2018-02-04 19:18:10 +0100

    Remove unused field

--- app/src/main/java/se/hactar/movieregister/viewmodel/MovieListViewModel.java
+++ app/src/main/java/se/hactar/movieregister/viewmodel/MovieListViewModel.java
@@ -2,7 +2,6 @@ package se.hactar.movieregister.viewmodel;
 
 
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
 import java.util.List;
@@ -11,7 +10,6 @@ import se.hactar.movieregister.MovieApp;
 import se.hactar.movieregister.model.Movie;
 
 public class MovieListViewModel extends ViewModel {
-    private MutableLiveData<List<Movie>> liveDataMovies;
 
     public LiveData<List<Movie>> getLiveDataMovies() {
         return MovieApp.getDb().movieDao().getAllLiveData();
